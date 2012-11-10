b9bca02ea296fdeffb67e6671ceab24a6559a963
mikool <mikool@mikools-MacBook-Air.local>
2012-11-09 18:24:18 -0800

    barter page shows current barter

--- client/barter.js
+++ client/barter.js
@@ -8,11 +8,29 @@ Template.barter.barters = function() {
 	return Barters.find({});
 };
 
-
+Template.barter.helpers({
+	title2: function() {
+		/*
+		if(Session.get('barter_title')) return Session.get('barter_title');
+		else return 'no title';*/
+		return Session.get('barter_title');
+	},
+	description2: function() {
+		return Session.get('barter_description');
+	}
+});
 
 Template.barter.events({
 	'click input': function() {
 		Barters.insert({title: $('#title').val(), description: $('#description').val()});
+	},
+	'click #show_barter': function() {
+		
+	},
+	'click .barter_item': function() {
+		//console.log(this);
+		Session.set('barter_title', this.title);
+		Session.set('barter_description', this.description);
 	}
 })
 
